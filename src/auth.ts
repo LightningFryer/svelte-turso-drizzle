619cf22929fcc47cd7345cbae5770818a1e4c833
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2024-04-23 00:07:47 +0530

    Added callback to fetch user id using jwt session strategy
    We need to use the jwt session strategy here in order to retrieve the user's id using OAuth and then update it to the database. By default the session strategy for databases are "database" which does not allow jwt.

--- src/auth.ts
+++ src/auth.ts
@@ -6,4 +6,17 @@ import google from "@auth/sveltekit/providers/google"
 export const { handle, signIn, signOut } = SvelteKitAuth({
   adapter: DrizzleAdapter(db),
   providers: [google],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      session.user.id = token.id as string
+      return session
+    },
+  },
+  session: {strategy: "jwt"}
 })
